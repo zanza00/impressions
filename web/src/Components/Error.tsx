7f2e1e306509acf6271bccf09f553a78e1288192
Simone Picciani <zanza00@gmail.com>
2019-08-01 12:56:33 +0200

    prettify error page

--- web/src/Components/Error.tsx
+++ web/src/Components/Error.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { Button } from "rsuite";
 
 import "./Error.css";
 
 const Error: React.FC = () => {
   return (
     <div className="Error">
-      Something went wrong{" "}
-      <span role="img" aria-label="face with open mouth and cold sweat">
-        😰
-      </span>
+      <div>
+        <h2>
+          Something went wrong{"  "}
+          <span role="img" aria-label="face with open mouth and cold sweat">
+            😰
+          </span>
+        </h2>
+      </div>
+      <div>
+        <Button onClick={() => window.location.reload()}>Reload</Button>
+      </div>
     </div>
   );
 };
