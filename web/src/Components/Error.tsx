c918c24bc99c727191909722f9a0cec1e32a11a8
Simone Picciani <zanza00@gmail.com>
2019-08-01 11:55:58 +0200

    fix emoji

--- web/src/Components/Error.tsx
+++ web/src/Components/Error.tsx
@@ -3,7 +3,14 @@ import React from "react";
 import "./Error.css";
 
 const Error: React.FC = () => {
-  return <div className="Error">Something went wrong 😰</div>;
+  return (
+    <div className="Error">
+      Something went wrong{" "}
+      <span role="img" aria-label="face with open mouth and cold sweat">
+        😰
+      </span>
+    </div>
+  );
 };
 
 export default Error;
